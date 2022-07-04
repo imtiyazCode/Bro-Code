62b855653e97ff80b5000fb5be5ce6ed0edd345b
Imtiyaz <76092300+IntiTalent@users.noreply.github.com>
2022-07-04 17:26:17 +0530

    retrived ChannelListContainer into prev form because of some bugs

--- client/src/Components/ChannelListContainer.jsx
+++ client/src/Components/ChannelListContainer.jsx
@@ -1,119 +1,55 @@
 import React from 'react';
-import { List, ListItem, ListItemAvatar, Avatar, Typography, Grid, Box, styled } from '@mui/material'
-import { ChannelList, useChatContext } from 'stream-chat-react';
+import { List, ListItem, ListItemAvatar, Avatar, Typography, Grid, Box } from '@mui/material'
 
 import { FiLogOut } from 'react-icons/fi';
 import { FaLaptopCode } from 'react-icons/fa'
 
-import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
-
-const SidebarMenuContainer = styled(List)(()=>({
-    width: '72px',
-    backgroundColor: '#0048c5',
-    height: '100vh',
-    padding: '0'
-}))
-
-const SidebarAvatar = styled(Avatar)(() => ({
-    backgroundColor: 'white',
-    width: 44,
-    height: 44,
-    color: '#000',
-    cursor: 'pointer'
-}))
-
-const HeaderTypography = styled(Typography)(() => ({
-    fontFamily: 'Helvetica Neue, sans-serif',
-    fontWeight: 'bold',
-    lineHeight: '28px',
-    fontSize: '18px',
-    color: "#fff"
-}))
-
-const ChannelListWrapper = styled(Box)(() => ({
-    display: 'flex',
-    flexDirection: 'column',
-    width: '240px',
-    backgroundColor:'#005fff',
-}))
+import { ChannelSearch } from './'
 
 const Sidebar = () => (
-    <SidebarMenuContainer >
-        <ListItem sx={{ paddingTop: '12px' }}>
+    <List sx={{ width: '72px', bgcolor: '#0048c5', height:'100vh', padding:'0'}} >
+        <ListItem sx={{paddingTop:'12px'}}>
             <ListItemAvatar>
-                <SidebarAvatar >
+                <Avatar sx={{ bgcolor: '#ffffff', width: 44, height: 44, color: '#000', cursor: 'pointer' }}>
                     <FaLaptopCode />
-                </SidebarAvatar>
+                </Avatar>
             </ListItemAvatar>
         </ListItem>
         <ListItem>
             <ListItemAvatar>
-                <SidebarAvatar>
+                <Avatar sx={{ bgcolor: '#ffffff', width: 44, height: 44, color: '#000', cursor: 'pointer' }}>
                     <FiLogOut />
-                </SidebarAvatar>
+                </Avatar>
             </ListItemAvatar>
         </ListItem>
-    </SidebarMenuContainer>
+    </List>
 )
 
 const CompanyHeader = () => (
     <Box display='flex' alignItems="center" height={62} paddingLeft='16px'>
-        <HeaderTypography variant="h1" component="p">
+        <Typography variant="h1" component="p" sx={{ fontFamily: 'Helvetica Neue, sans-serif', fontWeight: 'bold', lineHeight: '28px', fontSize: '18px', color:"#fff" }}>
             Bro Code
-        </HeaderTypography>
+        </Typography>
     </Box>
 )
 
-const customChannelTeamFilter = (channels) => {
-    return channels.filter((channel) => channel.type === 'team');
-}
-
-const customChannelMessagingFilter = (channels) => {
-    return channels.filter((channel) => channel.type === 'messaging');
-}
-
-
 const ChannelListContent = () => {
-
-    const { client } = useChatContext();
-
-    const filters = { members: { $in: [client.userID] } };
-
     return (
-        <Grid container sx={{maxHeight: '100%'}}>
+        <Grid container height='100%'>
             <Grid item xs="auto" height='100%'>
                 <Sidebar />
             </Grid>
-            <ChannelListWrapper item xs="auto" sx={{maxHeight: '100vh', bgcolor:'#005fff'}}>
+            <Box item xs
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    width: '240px',
+                    bgcolor: '#005fff'
+                }}
+            >
                 <CompanyHeader />
                 <ChannelSearch />
-                <ChannelList
-                    filters={filters}
-                    channelRenderFilterFn={customChannelTeamFilter}
-                    List={(listProps) => {
-                        <TeamChannelList
-                            {...listProps}
-                            type="team" />
-                    }}
-                    Preview={(previewProps) => {
-                        <TeamChannelPreview
-                            {...previewProps} />
-                    }}
-                />
-                <ChannelList
-                    filters={filters}
-                    channelRenderFilterFn={customChannelMessagingFilter}
-                    List={(listProps) => {
-                        <TeamChannelList
-                            {...listProps}
-                            type="messaging" />
-                    }}
-                    Preview={(previewProps) => {
-                        <TeamChannelPreview
-                            {...previewProps} />
-                    }}
-                />
-            </ChannelListWrapper>
+            </Box>
         </Grid>
     )
 }
